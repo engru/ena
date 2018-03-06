11cb3adf745b4710f34cb48c6605e349360778c7
lsytj0413 <511121939@qq.com>
2018-03-06 20:53:53 +0800

    package logger

--- atexit/atexit.go
+++ atexit/atexit.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package atexit provide a way to run Handler at process exit
 package atexit
 
 import (
