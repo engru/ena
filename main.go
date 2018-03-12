28df0ef22ac4713c5db3f5c3868f7008e8c67f40
lsytj0413 <511121939@qq.com>
2018-03-12 19:37:56 +0800

    conc.array

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/lsytj0413/deustgo/atexit"
+	"github.com/lsytj0413/deustgo/conc"
 )
 
 func main() {
@@ -12,5 +14,28 @@ func main() {
 	})
 
 	atexit.HandleInterrupts()
-	atexit.Exit(0)
+	// atexit.Exit(0)
+
+	arr := conc.NewConcurrentArray(10)
+	l := 10
+	var wg sync.WaitGroup
+	wg.Add(l)
+	for i := 0; i < l; i++ {
+		go func(i int) {
+			defer wg.Done()
+
+			for j := 0; j < 1000; j++ {
+				err := arr.Set(uint32(i), j)
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+		}(i)
+	}
+
+	wg.Wait()
+
+	for i := 0; i < l; i++ {
+		fmt.Println(arr.Get(uint32(i)))
+	}
 }
