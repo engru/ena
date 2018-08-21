a485f6d3cbf9185291271a0093b7440c2d9d4f13
lsytj0413 <511121939@qq.com>
2018-08-21 15:33:47 +0800

    example/profile stage 1

---
+++ example/profile/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"regexp"
+)
+
+var visitors int
+var colorRx = regexp.MustCompile(`^\w*$`)
+
+func handleHi(w http.ResponseWriter, r *http.Request) {
+	if match, _ := regexp.MatchString(`^\w*$`, r.FormValue("color")); !match {
+		http.Error(w, "Optional color is valid", http.StatusBadRequest)
+		return
+	}
+	visitors++
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Write([]byte("<h1 style='color: " + r.FormValue("color") + "'>Welcome!</h1>You are visitor number " + fmt.Sprint(visitors) + "!"))
+	return
+}
+
+func main() {
+	log.Printf("Starting on port 8080")
+	http.HandleFunc("/hi", handleHi)
+	log.Fatal(http.ListenAndServe("127.0.0.1:8080", nil))
+}
