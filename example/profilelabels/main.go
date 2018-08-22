d57023f9c8e739b4f91b705793f2a24f92b76e85
lsytj0413 <511121939@qq.com>
2018-08-22 15:22:03 +0800

    example/profilelables 2

--- example/profilelabels/main.go
+++ example/profilelabels/main.go
@@ -2,44 +2,36 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"log"
-	"math/rand"
 	"net/http"
-	"runtime/pprof"
-	"sync/atomic"
-
 	_ "net/http/pprof"
+	"runtime/pprof"
+	"time"
 )
 
-var i uint32
-
-func update(ctx context.Context) {
-	for j := 0; j < 100; j++ {
-		atomic.AddUint32(&i, 10)
-	}
-}
-
 func main() {
-	http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
-		labels := pprof.Labels("httppath", "/user")
-		pprof.Do(context.TODO(), labels, func(ctx context.Context) {
-			i := rand.Int31()
-			data, _ := json.Marshal(struct {
-				ID uint64
-			}{
-				ID: uint64(i),
+	ctx := context.Background()
+	go func() {
+		time.Sleep(time.Second)
+		for i := 0; i < 1000000000000; i++ {
+			labels := pprof.Labels("handler", "hello")
+			pprof.Do(ctx, labels, func(ctx context.Context) {
+				generate(1, 10)
 			})
-			w.Write(data)
-		})
-	})
-	http.HandleFunc("/message", func(w http.ResponseWriter, r *http.Request) {
-		labels := pprof.Labels("worker", "purge")
-		pprof.Do(context.TODO(), labels, func(ctx context.Context) {
-			go update(ctx)
-		})
+		}
+	}()
 
-		w.Write([]byte("/message"))
-	})
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	go func() {
+		time.Sleep(time.Second)
+		for i := 0; i < 1000000000000; i++ {
+			labels := pprof.Labels("handler2", "hello")
+			pprof.Do(ctx, labels, func(ctx context.Context) {
+				generate(1, 10)
+			})
+		}
+	}()
+
+	log.Fatal(http.ListenAndServe("localhost:5555", nil))
 }
+
+func generate(duration int, usage int) { /* string concats in loops */ }
