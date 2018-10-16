b7aa06523b15e3693ad857cfb0c08dd1beff8bc3
lsytj0413 <511121939@qq.com>
2018-10-16 15:20:21 +0800

    feat(algo): circular deque implement

--- algo/queue/circular_deque.go
+++ algo/queue/circular_deque.go
@@ -14,6 +14,8 @@
 
 package queue
 
+import "fmt"
+
 // CircularDeque is interface for CircularDeque Data Structure
 type CircularDeque interface {
 	Empty() bool
@@ -26,4 +28,127 @@ type CircularDeque interface {
 	PeekBack() interface{}
 	PushFront(interface{}) bool
 	PushBack(interface{}) bool
+}
+
+type circularDeque struct {
+	datas []interface{}
+
+	start int
+	end   int
+	size  int
+}
+
+func (c *circularDeque) PushFront(v interface{}) bool {
+	if c.IsFull() {
+		return false
+	}
+
+	c.size++
+	c.start--
+	if c.start < 0 {
+		c.start = len(c.datas) - 1
+	}
+	c.datas[c.start] = v
+	return true
+}
+
+func (c *circularDeque) PushBack(v interface{}) bool {
+	if c.IsFull() {
+		return false
+	}
+
+	c.size++
+	c.datas[c.end] = v
+	c.end++
+	if c.end >= len(c.datas) {
+		c.end = 0
+	}
+	return true
+}
+
+func (c *circularDeque) PeekBack() interface{} {
+	if c.Empty() {
+		return nil
+	}
+
+	if c.end == 0 {
+		return c.datas[len(c.datas)-1]
+	}
+	return c.datas[c.end-1]
+}
+
+func (c *circularDeque) PeekFront() interface{} {
+	if c.Empty() {
+		return nil
+	}
+
+	return c.datas[c.start]
+}
+
+func (c *circularDeque) PollBack() interface{} {
+	if c.Empty() {
+		return nil
+	}
+
+	c.size--
+	if c.end == 0 {
+		c.end = len(c.datas) - 1
+	} else {
+		c.end--
+	}
+	v := c.datas[c.end]
+	c.datas[c.end] = nil
+
+	return v
+}
+
+func (c *circularDeque) PollFront() interface{} {
+	if c.Empty() {
+		return nil
+	}
+
+	c.size--
+	v := c.datas[c.start]
+	c.datas[c.start] = nil
+
+	c.start++
+	if c.start >= len(c.datas) {
+		c.start = 0
+	}
+	return v
+}
+
+func (c *circularDeque) IsFull() bool {
+	return c.size == len(c.datas)
+}
+
+func (c *circularDeque) Empty() bool {
+	return c.size == 0
+}
+
+func (c *circularDeque) Cap() int {
+	return len(c.datas)
+}
+
+func (c *circularDeque) Len() int {
+	return c.size
+}
+
+const (
+	minCircularDequeCap int = 1
+	maxCircularDequeCap int = 10000
+)
+
+// NewCircularDeque will construct CircularDeque object
+func NewCircularDeque(cap int) (CircularDeque, error) {
+	if cap < minCircularDequeCap || cap > maxCircularDequeCap {
+		return nil, fmt.Errorf("Cap should within [%d, %d]", minCircularDequeCap, maxCircularDequeCap)
+	}
+
+	return &circularDeque{
+		datas: make([]interface{}, cap, cap),
+		start: 0,
+		end:   0,
+		size:  0,
+	}, nil
 }
